f78d63ab07bdb99b408558049cede662db7853a6
GeorgStubenrauch <georg.stubenrauch@gmx.de>
2016-01-15 17:01:56 +0100

    Fixed dynamic tooltip
    Fixed dynamic tooltip in function drawChoroplethMap, to only show the
    right namber of tooltips.

--- js/_Backup/choropleth_map_2.js
+++ js/_Backup/choropleth_map_2.js
@@ -1,11 +1,12 @@
 function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip,domain,range) {
-  var tooltip_length = attributes_tooltip.length;
+	console.log(attributes_tooltip.length);
   var height = 1024;
   var width = 768;
   var projection = d3.geo.mercator();
   var counties_var = void 0;
   var db = d3.map();
   var b, s, t;
+  //var sparkline = d3.charts.sparkline().height(50).width(138);
   var map = void 0; // update global
 
   var geoID = function(d) {
@@ -19,27 +20,39 @@ function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip
   var path = d3.geo.path().projection(projection);
   
   //Tooltip:
+  /*var divs_tooltip = [];
+  for (i=0; i<attributes_tooltip.length;i++) {	//attributes_tooltip.length <=6
+	//tooltip.append('div').attr('class', attributes_tooltip[i]);
+	divs_tooltip[i] = "." + attributes_tooltip[i];
+  }*/
    var tip = d3.tip()
 	.attr('class', 'd3-tip')
 	.offset([-10, 0])
 	.html(function(d) {
+		console.log(attributes_tooltip.length);
 		var tooltip = "";
-		if (d[attributes_tooltip[0]] != undefined) {
+		if (attributes_tooltip.length >= 1) {
+			console.log("1");
 			tooltip += attributes_tooltip[0] + ": " + d[attributes_tooltip[0]]; 
 		}
-        if (d[attributes_tooltip[1]] != undefined) {
+        if (attributes_tooltip.length >= 2) {
+			console.log("2");
 			tooltip += "<br>" + attributes_tooltip[1] + ": " + d[attributes_tooltip[1]];
 		}
-		if (d[attributes_tooltip[2]] != undefined) {
+		if (attributes_tooltip.length >= 3) {
+			console.log("3");
 			tooltip += "<br>" + attributes_tooltip[2] + ": " + d[attributes_tooltip[2]];
 		}
-		if (d[attributes_tooltip[3]] != undefined) {
+		if (attributes_tooltip.length >= 4) {
+			console.log("4");
 			tooltip += "<br>" + attributes_tooltip[3] + ": " + d[attributes_tooltip[3]];
 		}
-		if (d[attributes_tooltip[4]] != undefined) {
+		if (attributes_tooltip.length >= 5) {
+			console.log("5");
 			tooltip += "<br>" + attributes_tooltip[4] + ": " + d[attributes_tooltip[4]];
 		}
-		if (d[attributes_tooltip[5]] != undefined) {
+		if (attributes_tooltip.length = 6) {
+			console.log("6");
 			tooltip += "<br>" + attributes_tooltip[5] + ": " + d[attributes_tooltip[5]];
 		}
 		return tooltip;
@@ -56,7 +69,34 @@ function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip
     svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
   }
   
-  svg.call(tip);  
+  svg.call(tip);
+  
+  
+  
+  /*if (divs_tooltip.length > 0 & attributes_tooltip.length > 0) {
+	var tooltip = d3.select(div);
+	tooltip.append('div').attr('class','tooltip');
+	//var tooltip = d3.select(div).append('div').attr('class', 'tooltip');
+		if (divs_tooltip.length >= 1) {
+			tooltip.append('div').attr('class', divs_tooltip[0]);
+		}
+        if (divs_tooltip.length >= 2) {
+			tooltip.append('div').attr('class', divs_tooltip[1]);
+		}
+		if (divs_tooltip.length >= 3) {
+			tooltip.append('div').attr('class', divs_tooltip[2]);
+		}
+		if (divs_tooltip.length >= 4) {
+			tooltip.append('div').attr('class', divs_tooltip[3]);
+		}
+		if (divs_tooltip.length >= 5) {
+			tooltip.append('div').attr('class', divs_tooltip[4]);
+		}
+		if (divs_tooltip.length = 6) {
+			tooltip.append('div').attr('class', divs_tooltip[5]);
+		}
+  }*/
+  
 
   d3.json(filenames[0], function(data) {
 	d3.csv(filenames[1], function(statistics) {
@@ -137,6 +177,9 @@ function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip
 	
 	d3.csv(filenames[2], function(point_data) {
       var measurementPoints = svg.selectAll('circle').data(point_data);
+	  /* console.log("Bevor saveTooltips");
+	  Get Tooltips for measurements:
+	  var tooltip_dictionary = saveTooltips(raumluft_data,attributes_tooltip);*/
 
       measurementPoints.enter()
           .append('circle')
@@ -147,7 +190,34 @@ function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip
 		  .attr('stroke', 'black')
 		  //new tooltip:
 		  .on('mouseover', tip.show)			// event for tooltip
-		  .on('mouseout', tip.hide);			// event for tooltip  
+		  .on('mouseout', tip.hide);			// event for tooltip
+		  /*.on('mouseover', function(d) {
+				console.log("inMouseOver");
+				if (divs_tooltip.length >= 1) {
+					console.log(d[attributes_tooltip[0]]);
+					tooltip.select(divs_tooltip[0]).html(d[attributes_tooltip[0]]);
+				}
+				if (divs_tooltip.length >= 2) {
+					tooltip.select(divs_tooltip[1]).html(d[attributes_tooltip[1]]);
+				}
+				if (divs_tooltip.length >= 3) {
+					tooltip.select(divs_tooltip[2]).html(d[attributes_tooltip[2]]);
+				}
+				if (divs_tooltip.length >= 4) {
+					tooltip.select(divs_tooltip[3]).html(d[attributes_tooltip[3]]);
+				}
+				if (divs_tooltip.length >= 5) {
+					tooltip.select(divs_tooltip[4]).html(d[attributes_tooltip[4]]);
+				}
+				if (divs_tooltip.length = 6) {
+					tooltip.select(divs_tooltip[5]).html(d[attributes_tooltip[5]]);
+				}
+				tooltip.style('display', 'block');
+          }); 
+		  
+          raumluftPoints.on('mouseout', function() {                              // NEW
+            tooltip.style('display', 'none');
+          });*/  
     
 	});
 	
@@ -155,3 +225,23 @@ function drawChoroplethMap(div,filenames,attribute_choropleth,attributes_tooltip
 	
   });
 }
+
+/*function saveTooltips(d,attributes_tooltip) {
+	//console.log(d);
+	var tooltip_dictionary = {};
+	
+	d.forEach(function(data) {
+		tooltip = "";
+		for (i=0;attributes_tooltip.length;i++) {
+			tooltip += attributes_tooltip[i] + ": " +  data[attributes_tooltip[i]] + "<br>";
+		}
+		console.log(tooltip);
+		tooltip_dictionary[data] = tooltip;
+	});
+	
+	/*for (i=0;attributes_tooltip.length;i++) {
+		console.log(tooltip);
+		tooltip += attributes_tooltip[i] + ": " +  d[attributes_tooltip[i]] + "<br>";
+	}*/
+	/*return tooltip_dictionary;
+}*/
