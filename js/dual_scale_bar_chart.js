89252192a3c5b5d7a0ff1b48704e718e3f07af11
GeorgStubenrauch <georg.stubenrauch@gmx.de>
2016-01-15 10:58:27 +0100

    Function generalization
    Dual Scale Bar chart

--- js/dual_scale_bar_chart.js
+++ js/dual_scale_bar_chart.js
@@ -1,4 +1,5 @@
-function drawDualScaleBarChart(div) {
+function drawDualScaleBarChart(div,filename,attributes) { 
+/*div to append the svg element to, filename of the data, attributes to visualize [x-axis and y-axis; first: attribute for x-Axis, second: attribute for y-Axis (left bars), second: attribute for y-Axis (right bars)]*/
 	
 var margin = {top: 50, right: 80, bottom: 200, left: 120},
     width = 900 - margin.left - margin.right,
@@ -16,20 +17,20 @@ var yAxisLeft = d3.svg.axis().scale(y0).ticks(6).orient("left");
 // create right yAxis - Bodenluft:
 var yAxisRight = d3.svg.axis().scale(y1).ticks(6).orient("right");
 
-// Tooltip for Raumluft:
-var tip_rl = d3.tip()
+ // Tooltip for left bars:
+var tip_lb = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>AVG RL MW in </strong>" + d.NAME_2 +  "<strong>: </strong><span style='color:red'>" + d.AVG_MW_RL + "</span>" + "<strong> Bq m-3 </strong>";
+    return "<strong>Value in </strong>" + d.value_x +  "<strong>: </strong><span style='color:red'>" + d.value_y1 + "</span>" + "<strong> kBq m-3 </strong>";
   });
-  
- // Tooltip for Bodenluft:
-var tip_bl = d3.tip()
+
+// Tooltip for right bars:
+var tip_rb = d3.tip()		
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>AVG BL MW in </strong>" + d.NAME_2 +  "<strong>: </strong><span style='color:red'>" + d.AVG_MW_BL + "</span>" + "<strong> kBq m-3 </strong>";
+    return "<strong>Value in </strong>" + d.value_x +  "<strong>: </strong><span style='color:red'>" + d.value_y2 + "</span>" + "<strong> Bq m-3 </strong>";
   });
   
 var svg = d3.select(div).append("svg")
@@ -38,13 +39,13 @@ var svg = d3.select(div).append("svg")
   .append("g")
     .attr("class", "graph")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
-svg.call(tip_rl);
-svg.call(tip_bl);
 
-d3.csv("../data/processed_data/adm/DEU_adm2_pa_clip_total_statistics_fixed.csv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.NAME_2; }));
-  y0.domain([0, d3.max(data, function(d) { return d.AVG_MW_RL; })]); //average raumluft measurement in county
+svg.call(tip_lb);	
+svg.call(tip_rb);
+
+d3.csv(filename, type, function(error, data) {
+  x.domain(data.map(function(d) { return d.value_x; }));
+  y0.domain([0, d3.max(data, function(d) { return d.value_y1; })]); //average raumluft measurement in county
   
   svg.append("g")
       .attr("class", "x axis")
@@ -98,26 +99,28 @@ d3.csv("../data/processed_data/adm/DEU_adm2_pa_clip_total_statistics_fixed.csv",
   bars = svg.selectAll(".bar").data(data).enter();
   bars.append("rect")
       .attr("class", "bar1")
-      .attr("x", function(d) { return x(d.NAME_2); })
+      .attr("x", function(d) { return x(d.value_x); })
       .attr("width", x.rangeBand()/2)
-      .attr("y", function(d) { return y0(d.AVG_MW_RL); })
-	  .attr("height", function(d,i,j) { return height - y0(d.AVG_MW_RL); }) //;
+      .attr("y", function(d) { return y0(d.value_y1); })
+	  .attr("height", function(d,i,j) { return height - y0(d.value_y1); }) //;
 	  //new tooltip:
-      .on('mouseover', tip_rl.show)										// event for tooltip
-      .on('mouseout', tip_rl.hide);										// event for tooltip
+      .on('mouseover', tip_lb.show)										// event for tooltip
+      .on('mouseout', tip_lb.hide);										// event for tooltip
 	  
   bars.append("rect")
       .attr("class", "bar2")
-      .attr("x", function(d) { return x(d.NAME_2) + x.rangeBand()/2; })
+      .attr("x", function(d) { return x(d.value_x) + x.rangeBand()/2; })
       .attr("width", x.rangeBand() / 2)
-      .attr("y", function(d) { return y1(d.AVG_MW_BL); })
-	  .attr("height", function(d,i,j) { return height - y1(d.AVG_MW_BL); }) //; 
+      .attr("y", function(d) { return y1(d.value_y2); })
+	  .attr("height", function(d,i,j) { return height - y1(d.value_y2); }) //; 
 	  //new tooltip:
-      .on('mouseover', tip_bl.show)										// event for tooltip
-      .on('mouseout', tip_bl.hide);										// event for tooltip
+      .on('mouseover', tip_rb.show)										// event for tooltip
+      .on('mouseout', tip_rb.hide);										// event for tooltip
 });
 function type(d) {
-  d.AVG_MW_RL = +d.AVG_MW_RL;
+  d.value_x = d[attributes[0]];
+  d.value_y1 = +d[attributes[1]];
+  d.value_y2 = +d[attributes[2]];
   return d;
 }
 }
